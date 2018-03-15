4dc2350b5f5c4790146986d8cd0bbe78eb9b35fd
Liana Mancini <liana.mancini@gmail.com>
2018-03-14 23:51:33 -0400

    fix user input bug to have ONLY ONE USER INPUT ID >.<

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var topics = ["bird", "cat", "dog"];
     // Dynamically build buttons based on topics array
-    
+
     function renderButtons() {
         $("#button-container").empty();
         for (var i = 0; i < topics.length; i++) {
@@ -23,53 +23,59 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
             console.log(response.data.length);
+            var newCollection, newRow, newCol, image, imageSource, animate, rating, ratingP;
             for (var i = 0; i < response.data.length; i++) {
-                if (i % 4 === 0){
+                if (i === 0) {
+                    newCollection = $("<div class='container'>");
+                }
+                if (i % 4 === 0) {
+                    newRow = $("<div class='row'>")
+                    $(newCollection).append(newRow);
                     // console.log("row" + i);
-                    var newRow = $("<div class='row'>")
-                    $("#gif-container").append(newRow);    
                 }
                 // console.log("col" + i);
 
-                var newCol = $("<div>");
+                newCol = $("<div>");
                 $(newCol).attr("class", "col-md-3");
 
-                var image = $("<img>");
-                var imageSource = response.data[i].images.fixed_height_still.url;
-                var animate = response.data[i].images.fixed_height.url;
+                image = $("<img>");
+                imageSource = response.data[i].images.fixed_height_still.url;
+                animate = response.data[i].images.fixed_height.url;
                 $(image).attr("src", imageSource);
                 $(image).attr("data-still", imageSource);
                 $(image).attr("data-animate", animate);
                 $(image).attr("data-state", "still");
                 $(image).addClass("gif");
 
-                var rating = response.data[i].rating;
-                var ratingP = $("<p>");
+                rating = response.data[i].rating;
+                ratingP = $("<p>");
                 $(ratingP).text(rating.toUpperCase());
 
                 $(newCol).append(image);
                 $(newCol).append(ratingP);
                 $(newRow).append(newCol);
             }
+            $("#gif-container").prepend(newCollection);
         });
     }
 
-    function playGif(){
+    function playGif() {
         console.log("click");
-        if($(this).attr("data-state") === "still"){
+        if ($(this).attr("data-state") === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
-          } else {
+        } else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
-          }
+        }
     }
 
     $(document.body).on("click", ".topic", getGif);
     $(document.body).on("click", ".gif", playGif);
 
-    $("#submit-topic").on("click", function(event){
+    $("#submit-topic").on("click", function (event) {
         event.preventDefault();
+        // var userInput = $("#user-input").val();
         var userInput = $("#user-input").val();
         console.log(userInput);
         topics.push(userInput);
